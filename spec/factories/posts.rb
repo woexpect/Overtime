aebbf895b507112a562f2d32458151a262f1d747
Johan Peña <johan@fullstacklabs.co>
2020-09-09 12:57:57 -0500

    Added permissons structure and implemented new index query for only retrieving the users own posts

--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -6,8 +6,14 @@ FactoryGirl.define do
     end
 
     factory :second_post, class: "Post" do
-        date Date.today
+        date Date.yesterday
         rationale "Some more content"
         user
+    end
+
+    factory :post_from_other_user, class: "Post" do
+        date Date.yesterday
+        rationale "Some more content"
+        non_authorized_user
     end
 end 
