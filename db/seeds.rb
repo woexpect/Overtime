996220598b8f82177ec9f0262a0878de5616cd82
Johan Peña <johan@fullstacklabs.co>
2020-09-14 09:48:36 -0500

    Added reqeusted overtime to view, model and db.

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,7 @@ AdminUser.create(email: "admin2@test.com", password: "asdasd", password_confirma
 puts "1 Admin User created"
 
 100.times do |posts|
-    Post.create!(date: Date.today, rationale: "#{posts} rationale content", user_id: @user.id)
+    Post.create!(date: Date.today, rationale: "#{posts} rationale content", user_id: @user.id, overtime_request: 2.5)
 end
 
 puts "100 Posts have been created"
